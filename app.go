0c0e7e3158eb8d41f381a6f1cbf92d45a9dc44b4
Ahmad Husen <husenisme@gmail.com>
2020-10-07 23:19:53 +0700

    Add 404 handler

--- app.go
+++ app.go
@@ -5,11 +5,19 @@ import (
 )
 
 func main() {
+	// Fiber instance
 	app := fiber.New()
 
+	// Routes
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello world!")
 	})
 
+	// 404 handler
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404)
+	})
+
+	// Start server
 	app.Listen(":3000")
 }
