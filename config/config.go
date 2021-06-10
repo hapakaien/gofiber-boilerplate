04a44f7bb2894e23086d7e595f6245d1472080f0
Ahmad Husen <hi@husen.id>
2021-06-10 18:38:48 +0700

    chore: use composite literal as config value

--- config/config.go
+++ config/config.go
@@ -1,17 +1,41 @@
 package config
 
 import (
-	"fmt"
-	"os"
+	"github.com/gofiber/fiber/v2"
 
-	"github.com/joho/godotenv"
+	"github.com/confetti-framework/support/env"
 )
 
-// Config func to get env value
-func Config(key string) string {
-	// load .env file
-	if err := godotenv.Load(".env"); err != nil {
-		fmt.Printf("Error loading .env file")
-	}
-	return os.Getenv(key)
+var Fiber *fiber.Config
+
+var App = struct {
+	Name string
+	Url  string
+	Port int
+}{
+	Name: env.StringOr("APP_NAME", "Fiber"),
+	Url:  env.StringOr("APP_URL", "http://localhost"),
+	Port: env.IntOr("APP_PORT", 3000),
+}
+
+var Cors = struct {
+	Origins string
+}{
+	Origins: env.StringOr("CORS_ORIGINS", "*"),
+}
+
+var Database = struct {
+	Url      string
+	Host     string
+	Port     int
+	Name     string
+	Username string
+	Password string
+}{
+	Url:      env.String("DATABASE_URL"),
+	Host:     env.StringOr("DB_HOST", "127.0.0.1"),
+	Port:     env.IntOr("DB_PORT", 3306),
+	Name:     env.StringOr("DB_DATABASE", "fiber"),
+	Username: env.StringOr("DB_USERNAME", "fiber"),
+	Password: env.StringOr("DB_PASSWORD", ""),
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/hapakaien/fiber-boilerplate
 go 1.14
 
 require (
+	github.com/confetti-framework/support v0.4.0
 	github.com/gofiber/fiber/v2 v2.12.0
-	github.com/joho/godotenv v1.3.0
 	github.com/stretchr/testify v1.7.0
 	github.com/tidwall/gjson v1.8.0
 )
